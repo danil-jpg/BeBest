cc72708368f7c0fe1cdb614410f3a26ae6d9e0e8
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-08-02 07:53:25 +0300

    update margin last child filter inner

--- src/components/pages/Catalog/FilterCatalog/FilterCatalog.jsx
+++ src/components/pages/Catalog/FilterCatalog/FilterCatalog.jsx
@@ -10,7 +10,7 @@ import {
     setSex,
     setPreparingTest,
     setAspectLearn,
-    setOldLearn
+    setOldLearn,
 } from '../../../../store/slices/filterSlice/filterSlice';
 import RangeSlider from '../../../UI/Forms/RangeSlider/RangeSlider';
 
@@ -23,58 +23,60 @@ const FilterCatalog = (props) => {
     return (
         <form className='filter-catalog'>
             <h2 className='filter-catalog__title'>Фильтры</h2>
-            <div className='filter-catalog__inner'>
-                <SelectFormContainer
-                    title={data.lessonSpeak.title}
-                    list={data.lessonSpeak.list}
-                    setItem={setLessonSpeak}
-                />
+            <div className='filter-catalog__body'>
+                <div className='filter-catalog__inner'>
+                    <SelectFormContainer
+                        title={data.lessonSpeak.title}
+                        list={data.lessonSpeak.list}
+                        setItem={setLessonSpeak}
+                    />
 
-                <SelectFormContainer
-                    title={data.lessonTime.title}
-                    list={data.lessonTime.list}
-                    setItem={setLessonTime}
-                />
+                    <SelectFormContainer
+                        title={data.lessonTime.title}
+                        list={data.lessonTime.list}
+                        setItem={setLessonTime}
+                    />
 
-                <SelectFormContainer
-                    title={data.lessonCountry.title}
-                    list={data.lessonCountry.list}
-                    setItem={setLessonCountry}
-                />
+                    <SelectFormContainer
+                        title={data.lessonCountry.title}
+                        list={data.lessonCountry.list}
+                        setItem={setLessonCountry}
+                    />
 
-                <SelectFormContainer
-                    title={data.langSpeak.title}
-                    list={data.langSpeak.list}
-                    setItem={setLangSpeak}
-                />
-            </div>
-            <div className='filter-catalog__inner'>
-                <RangeSlider title={titlePrice} />
-            </div>
-            <div className='filter-catalog__inner'>
-                <SelectFormContainer
-                    title={data.sex.title}
-                    list={data.sex.list}
-                    setItem={setSex}
-                />
+                    <SelectFormContainer
+                        title={data.langSpeak.title}
+                        list={data.langSpeak.list}
+                        setItem={setLangSpeak}
+                    />
+                </div>
+                <div className='filter-catalog__inner'>
+                    <RangeSlider title={titlePrice} />
+                </div>
+                <div className='filter-catalog__inner'>
+                    <SelectFormContainer
+                        title={data.sex.title}
+                        list={data.sex.list}
+                        setItem={setSex}
+                    />
 
-                <SelectFormContainer
-                    title={data.preparingTest.title}
-                    list={data.preparingTest.list}
-                    setItem={setPreparingTest}
-                />
+                    <SelectFormContainer
+                        title={data.preparingTest.title}
+                        list={data.preparingTest.list}
+                        setItem={setPreparingTest}
+                    />
 
-                <SelectFormContainer
-                    title={data.aspectLearn.title}
-                    list={data.aspectLearn.list}
-                    setItem={setAspectLearn}
-                />
+                    <SelectFormContainer
+                        title={data.aspectLearn.title}
+                        list={data.aspectLearn.list}
+                        setItem={setAspectLearn}
+                    />
 
-                <SelectFormContainer
-                    title={data.oldLearn.title}
-                    list={data.oldLearn.list}
-                    setItem={setOldLearn}
-                />
+                    <SelectFormContainer
+                        title={data.oldLearn.title}
+                        list={data.oldLearn.list}
+                        setItem={setOldLearn}
+                    />
+                </div>
             </div>
         </form>
     );
