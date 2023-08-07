e8c802976dc1c832fe720cd326d31c17dda37a06
Danil <danil.polovinkin.1994@gmail.com>
2023-08-07 08:17:31 +0300

    completed sideblock

--- src/components/UI/Forms/RadioForm/RadioForm.jsx
+++ src/components/UI/Forms/RadioForm/RadioForm.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import s from './RadioForm.module.scss';
 import InputBody from '../InputBody/InputBody';
-import { isChecked } from '../../../utils/isActive';
+import { isActive } from '../../../../utils/isActive';
 
 const RadioForm = ({ name, text, title, value, checked, setChecked }) => {
-    return (
-        <label className={`${s.radio} ${isChecked(checked, value, s.active)}`}>
-            <input
-                className={`${s.radio__input}`}
-                type='radio'
-                name={name}
-                value={value}
-                checked={checked === value}
-                onChange={(e) => {
-                    setChecked(e.currentTarget.value);
-                }}
-            />
-            <div className={s.radio__custom}></div>
-            <InputBody title={title} text={text} />
-        </label>
-    );
+    // return (
+    //     <label className={`${s.radio} ${isChecked(checked, value, s.active)}`}>
+    //         <input
+    //             className={`${s.radio__input}`}
+    //             type='radio'
+    //             name={name}
+    //             value={value}
+    //             checked={checked === value}
+    //             onChange={(e) => {
+    //                 setChecked(e.currentTarget.value);
+    //             }}
+    //         />
+    //         <div className={s.radio__custom}></div>
+    //         <InputBody title={title} text={text} />
+    //     </label>
+    //     // тут баг,компоненты имЧекд - у нас на проекте не существует!,исАктив - не замена.
+    // );
 };
 
 export default RadioForm;
--- src/components/pages/Teachers/TeacherLessons/TeacherLesson.jsx
+++ src/components/pages/Teachers/TeacherLessons/TeacherLesson.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 import './TeacherLesson.scss';
 import MainButton from '../../../UI/Buttons/MainButton/MainButton';
 import RadioForm from '../../../UI/Forms/RadioForm/RadioForm';
+import clock from '../../../../assets/icons/smallClock.svg';
+import person from '../../../../assets/icons/person.svg';
+import cap from '../../../../assets/icons/cap.svg';
 
 const TeacherLesson = () => {
+    const x = () =>
+        Date.toLocaleString('default', {
+            month: 'long',
+        });
+    console.log(x);
     return (
         <div className='teacher__lessons'>
             <div className='teacher__trial'>
@@ -16,11 +24,11 @@ const TeacherLesson = () => {
                     <p className='teacher__trial__row_text'>Стоисмость курса</p>
                     <p className='teacher__trial__row_num'>1000 $ </p>
                 </div>
-                <MainButton type='middle' style={{ marginTop: '17px' }}>
+                <MainButton style={{ marginTop: '17px', textTransform: 'UpperCase' }}>
                     Забронировать
                 </MainButton>
             </div>
-            <div className='teacher__trial'>
+            <div className='teacher__trial teacher__trial_forLate'>
                 <p className='teacher__trial_text'>Индивидуальный урок</p>
                 <div className='teacher__trial__row'>
                     <RadioForm />
@@ -29,9 +37,206 @@ const TeacherLesson = () => {
                     <p className='teacher__trial__row_text'>Стоисмость курса</p>
                     <p className='teacher__trial__row_num'>1000 $ </p>
                 </div>
-                <MainButton type='middle' style={{ marginTop: '17px' }}>
+                <MainButton
+                    style={{
+                        marginTop: '17px',
+                        marginBottom: '7px',
+                        textTransform: 'UpperCase',
+                    }}>
                     Забронировать
                 </MainButton>
+            </div>
+            <div className='teacher__trial'>
+                <p className='teacher__trial_text'>Групповые уроки</p>
+                <div className='teacher__trial__box'>
+                    <p className='teacher__trial__box_date'>28 июня 2021</p>
+                    <p className='teacher__trial__box_title'>Базовые навыки испанского языка</p>
+                    <div className='teacher__trial__box__attributes '>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_red'>
+                            <img
+                                src={clock}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text '>1 час</p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_purple'>
+                            <img src={cap} className='teacher__trial__box__attributes__item_img' />
+                            <p className='teacher__trial__box__attributes__item_text'>Beginners</p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_sky'>
+                            <img
+                                src={person}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text'>10-15 лет</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__box__data'>
+                        <div className='teacher__trial__box__data__left'>
+                            <p className='teacher__trial__box__data_title'>Зарегистрировались</p>
+                            <p className='teacher__trial__box__data_text'>10 человек</p>
+                        </div>
+                        <div className='teacher__trial__box__data__right'>
+                            <p className='teacher__trial__box__data_title'>Максимум:</p>
+                            <p className='teacher__trial__box__data_text'>15 человек</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__row'>
+                        <p className='teacher__trial__row_text  teacher__trial__row_text_sm'>
+                            Стоисмость курса
+                        </p>
+                        <p className='teacher__trial__row_num'>1000 $ </p>
+                    </div>
+                    <MainButton
+                        style={{
+                            marginTop: '17px',
+                            marginBottom: '7px',
+                            textTransform: 'UpperCase',
+                        }}>
+                        Забронировать
+                    </MainButton>
+                </div>
+                <div className='teacher__trial__box'>
+                    <p className='teacher__trial__box_date'>28 июня 2021</p>
+                    <p className='teacher__trial__box_title'>Базовые навыки испанского языка</p>
+                    <div className='teacher__trial__box__attributes '>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_red'>
+                            <img
+                                src={clock}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text '>1 час</p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_purple'>
+                            <img src={cap} className='teacher__trial__box__attributes__item_img' />
+                            <p className='teacher__trial__box__attributes__item_text'>Beginners</p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_sky'>
+                            <img
+                                src={person}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text'>10-15 лет</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__box__data'>
+                        <div className='teacher__trial__box__data__left'>
+                            <p className='teacher__trial__box__data_title'>Зарегистрировались</p>
+                            <p className='teacher__trial__box__data_text'>10 человек</p>
+                        </div>
+                        <div className='teacher__trial__box__data__right'>
+                            <p className='teacher__trial__box__data_title'>Максимум:</p>
+                            <p className='teacher__trial__box__data_text'>15 человек</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__row'>
+                        <p className='teacher__trial__row_text teacher__trial__row_text_sm'>
+                            Стоисмость курса
+                        </p>
+                        <p className='teacher__trial__row_num'>1000 $ </p>
+                    </div>
+                    <MainButton
+                        style={{
+                            marginTop: '17px',
+                            marginBottom: '7px',
+                            textTransform: 'UpperCase',
+                        }}>
+                        Забронировать
+                    </MainButton>
+                </div>
+            </div>
+            <div className='teacher__trial'>
+                <p className='teacher__trial_text'>Учебные курсы</p>
+                <div className='teacher__trial__box'>
+                    <p className='teacher__trial__box_date'>28 июня 2021</p>
+                    <p className='teacher__trial__box_title'>Базовые навыки испанского языка</p>
+                    <div className='teacher__trial__box__attributes '>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_purple'>
+                            <img src={cap} className='teacher__trial__box__attributes__item_img' />
+                            <p className='teacher__trial__box__attributes__item_text'>
+                                Intermediate
+                            </p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_sky'>
+                            <img
+                                src={person}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text'>
+                                старше 16 лет
+                            </p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__box__data'>
+                        <div className='teacher__trial__box__data__left'>
+                            <p className='teacher__trial__box__data_title'>Зарегистрировались</p>
+                            <p className='teacher__trial__box__data_text'>10 человек</p>
+                        </div>
+                        <div className='teacher__trial__box__data__right'>
+                            <p className='teacher__trial__box__data_title'>Максимум:</p>
+                            <p className='teacher__trial__box__data_text'>15 человек</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__row'>
+                        <p className='teacher__trial__row_text  teacher__trial__row_text_sm'>
+                            Стоисмость курса
+                        </p>
+                        <p className='teacher__trial__row_num'>1000 $ </p>
+                    </div>
+                    <MainButton
+                        style={{
+                            marginTop: '17px',
+                            marginBottom: '7px',
+                            textTransform: 'UpperCase',
+                        }}>
+                        Забронировать
+                    </MainButton>
+                </div>
+                <div className='teacher__trial__box'>
+                    <p className='teacher__trial__box_date'>28 июня 2021</p>
+                    <p className='teacher__trial__box_title'>Уровень разговорного – Бог</p>
+                    <div className='teacher__trial__box__attributes '>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_purple'>
+                            <img src={cap} className='teacher__trial__box__attributes__item_img' />
+                            <p className='teacher__trial__box__attributes__item_text'>
+                                Intermediate
+                            </p>
+                        </div>
+                        <div className='teacher__trial__box__attributes__item teacher__trial__box__attributes__item_sky'>
+                            <img
+                                src={person}
+                                className='teacher__trial__box__attributes__item_img'
+                            />
+                            <p className='teacher__trial__box__attributes__item_text'>
+                                старше 16 лет
+                            </p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__box__data'>
+                        <div className='teacher__trial__box__data__left'>
+                            <p className='teacher__trial__box__data_title'>Зарегистрировались</p>
+                            <p className='teacher__trial__box__data_text'>10 человек</p>
+                        </div>
+                        <div className='teacher__trial__box__data__right'>
+                            <p className='teacher__trial__box__data_title'>Максимум:</p>
+                            <p className='teacher__trial__box__data_text'>15 человек</p>
+                        </div>
+                    </div>
+                    <div className='teacher__trial__row'>
+                        <p className='teacher__trial__row_text teacher__trial__row_text_sm'>
+                            Стоисмость курса
+                        </p>
+                        <p className='teacher__trial__row_num'>1000 $ </p>
+                    </div>
+                    <MainButton
+                        style={{
+                            marginTop: '17px',
+                            marginBottom: '7px',
+                            textTransform: 'UpperCase',
+                        }}>
+                        Забронировать
+                    </MainButton>
+                </div>
             </div>
         </div>
     );
