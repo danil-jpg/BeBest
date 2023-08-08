4c7def1064495c2df0ae9293f83d111f74ede9bb
Danil <danil.polovinkin.1994@gmail.com>
2023-08-08 14:11:52 +0300

    added whole logic to calendar

--- src/components/pages/Teachers/TeacherCalendar/TeacherCalendar.jsx
+++ src/components/pages/Teachers/TeacherCalendar/TeacherCalendar.jsx
@@ -1,74 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './TeacherCalendar.scss';
 import { v1 } from 'uuid';
+import axios from 'axios';
 
-const TeacherCalendar = ({ arr = [] }) => {
-    let obj = [
-        {
-            date: 21,
-            month: 'июнь',
-            time: [true, true, true, true, true, true, true, true, true, true],
-        },
-        {
-            date: 22,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-        {
-            date: 23,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-        {
-            date: 24,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-        {
-            date: 25,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-        {
-            date: 26,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-        {
-            date: 27,
-            month: 'июнь',
-            time: [false, false, false, false, false, false, true, true, true, false],
-        },
-    ];
+const TeacherCalendar = () => {
+    const [obj, setObj] = useState('');
+    let [weekCounter, setWeekCounter] = useState(0);
 
+    const fetchData = async () => {
+        const res = await axios
+            .get('http://bebest.pp.ua/api/calendars/')
+            .then((res) => setObj(res));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // console.log(obj?.data?.data[weekCounter].attributes.data);
     return (
         <div className='teacher__calendar'>
             <div className='teacher__calendar__wr'>
                 <div className='teacher__calendar_date-wr'>
-                    <div className='date_cursor date_cursor_left'></div>
+                    <div
+                        className='date_cursor date_cursor_left'
+                        onClick={() => setWeekCounter(0)}></div>
                     <div className='date_time-wr '>
                         <div className='date_time-wr_start '>
-                            <p className='date_day'>{obj[0].date}</p>
-                            <p className='date_month'>{obj[0].month}</p>
+                            <p className='date_day'>
+                                {obj?.data?.data[weekCounter].attributes.data[0].date}
+                            </p>
+                            <p className='date_month'>
+                                {obj?.data?.data[0].attributes.data[weekCounter].month}
+                            </p>
                             <p className='date_year'> 2023</p>
                         </div>
                         <p>----</p>
                         <div className='date_time-wr_end '>
-                            <p className='date_day'>{obj.at(-1).date}</p>
-                            <p className='date_month'>{obj.at(-1).month}</p>
+                            <p className='date_day'>
+                                {obj?.data?.data[weekCounter].attributes.data.at(-1).date}
+                            </p>
+                            <p className='date_month'>
+                                {obj?.data?.data[weekCounter].attributes.data.at(-1).month}
+                            </p>
                             <p className='date_year'>2023</p>
                         </div>
                     </div>
-                    <div className='date_cursor date_cursor_right'></div>
+                    <div
+                        className='date_cursor date_cursor_right'
+                        onClick={() => setWeekCounter(1)}></div>
                 </div>
                 <div className='teacher__calendar__wr-row'>
-                    {obj.map((el) => (
+                    {obj?.data?.data[weekCounter].attributes.data.map((el) => (
                         <div key={v1()} className='teacher__calendar__wr_date'>
                             <p className='teacher__calendar__wr_day'>{el.date}</p>
                             <p className='teacher__calendar__wr_month'>{el.month}</p>
                         </div>
                     ))}
-                    {obj.map((element) => (
+                    {obj?.data?.data[weekCounter].attributes.data.map((element) => (
                         <div className='teacher__calendar__wr_column' key={v1()}>
                             {element.time.map((el, index) => (
                                 <div
