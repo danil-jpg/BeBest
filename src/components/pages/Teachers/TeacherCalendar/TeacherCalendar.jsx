e2ebf5c96c1cf39529e8f1e6f575bd5071b3621c
Danil <danil.polovinkin.1994@gmail.com>
2023-08-08 15:19:10 +0300

    added some styles to calendar

--- src/components/pages/Teachers/TeacherCalendar/TeacherCalendar.jsx
+++ src/components/pages/Teachers/TeacherCalendar/TeacherCalendar.jsx
@@ -20,6 +20,7 @@ const TeacherCalendar = () => {
     // console.log(obj?.data?.data[weekCounter].attributes.data);
     return (
         <div className='teacher__calendar'>
+            <p className='teacher__calendar__title'>Расписание</p>
             <div className='teacher__calendar__wr'>
                 <div className='teacher__calendar_date-wr'>
                     <div
@@ -35,7 +36,7 @@ const TeacherCalendar = () => {
                             </p>
                             <p className='date_year'> 2023</p>
                         </div>
-                        <p>----</p>
+                        <p>-</p>
                         <div className='date_time-wr_end '>
                             <p className='date_day'>
                                 {obj?.data?.data[weekCounter].attributes.data.at(-1).date}
@@ -50,35 +51,49 @@ const TeacherCalendar = () => {
                         className='date_cursor date_cursor_right'
                         onClick={() => setWeekCounter(1)}></div>
                 </div>
-                <div className='teacher__calendar__wr-row'>
-                    {obj?.data?.data[weekCounter].attributes.data.map((el) => (
-                        <div key={v1()} className='teacher__calendar__wr_date'>
-                            <p className='teacher__calendar__wr_day'>{el.date}</p>
-                            <p className='teacher__calendar__wr_month'>{el.month}</p>
-                        </div>
-                    ))}
-                    {obj?.data?.data[weekCounter].attributes.data.map((element) => (
-                        <div className='teacher__calendar__wr_column' key={v1()}>
-                            {element.time.map((el, index) => (
-                                <div
-                                    onClick={() => {
-                                        if (el === true) {
-                                            alert(index + 8);
-                                        } else {
-                                            alert(index + 8);
+                <div className='teacher__calendar__calendar-wr'>
+                    <div className='teacher__calendar__calendar_subtime'>
+                        <p className='teacher__calendar_subtitle-text'>08:00</p>
+                        <p className='teacher__calendar_subtitle-text'>09:00</p>
+                        <p className='teacher__calendar_subtitle-text'>10:00</p>
+                        <p className='teacher__calendar_subtitle-text'>11:00</p>
+                        <p className='teacher__calendar_subtitle-text'>12:00</p>
+                        <p className='teacher__calendar_subtitle-text'>13:00</p>
+                        <p className='teacher__calendar_subtitle-text'>14:00</p>
+                        <p className='teacher__calendar_subtitle-text'>15:00</p>
+                        <p className='teacher__calendar_subtitle-text'>16:00</p>
+                        <p className='teacher__calendar_subtitle-text'>17:00</p>
+                    </div>
+                    <div className='teacher__calendar__wr-row'>
+                        {obj?.data?.data[weekCounter].attributes.data.map((el) => (
+                            <div key={v1()} className='teacher__calendar__wr_date'>
+                                <p className='teacher__calendar__wr_day'>{el.date}</p>
+                                <p className='teacher__calendar__wr_month'>{el.month}</p>
+                            </div>
+                        ))}
+                        {obj?.data?.data[weekCounter].attributes.data.map((element) => (
+                            <div className='teacher__calendar__wr_column' key={v1()}>
+                                {element.time.map((el, index) => (
+                                    <div
+                                        onClick={() => {
+                                            if (el === true) {
+                                                alert(index + 8);
+                                            } else {
+                                                alert(index + 8);
+                                            }
+                                        }}
+                                        className={
+                                            el === true
+                                                ? ' teacher__calendar__wr_item_filled'
+                                                : 'teacher__calendar__wr_item'
                                         }
-                                    }}
-                                    className={
-                                        el === true
-                                            ? ' teacher__calendar__wr_item_filled'
-                                            : 'teacher__calendar__wr_item'
-                                    }
-                                    key={v1()}>
-                                    {el.time}
-                                </div>
-                            ))}
-                        </div>
-                    ))}
+                                        key={v1()}>
+                                        {el.time}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
