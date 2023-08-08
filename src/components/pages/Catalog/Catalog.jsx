e05db3cebe38ce3d54712d46a7861e7e9ac7ba32
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-08-08 17:45:02 +0300

    add select styles

--- src/components/pages/Catalog/Catalog.jsx
+++ src/components/pages/Catalog/Catalog.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Catalog.scss';
 import ContainerMain from '../../common/ContainerMain/ContainerMain';
 import Title from '../../UI/Title/Title';
@@ -6,15 +6,36 @@ import FilterCatalog from './FilterCatalog/FilterCatalog';
 import UserListCatalog from './UserListCatalog/UserListCatalog';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../../../store/slices/catalogSlice/catalogSlice';
-import axios from 'axios';
 import CatalogArticles from './CatalogArticles/CatalogArticles';
+import RadioForm from '../../UI/Forms/RadioForm/RadioForm';
+import { v1 } from 'uuid';
 
 const data = {
     title: 'Список преподавателей',
     titleArticles: 'Полезные статьи',
 };
 
+const testRadio = [
+    {
+        id: v1(),
+        title: '1',
+        value: 'first',
+    },
+    {
+        id: v1(),
+        title: '2',
+        value: 'second',
+    },
+    {
+        id: v1(),
+        title: '3',
+        value: 'third',
+    },
+];
+
 const Catalog = (props) => {
+    const [checked, setChecked] = useState('first');
+    
     let dispatch = useDispatch();
 
     useEffect(() => {
@@ -33,6 +54,18 @@ const Catalog = (props) => {
                         <div className='catalog__body'>
                             <UserListCatalog />
                         </div>
+                    </div>
+                    <div className='catalog__radio'>
+                        {testRadio.map((el) => (
+                            <RadioForm
+                                key={el.id}
+                                title={el.title}
+                                value={el.value}
+                                name='testRadio'
+                                checked={checked}
+                                setChecked={setChecked}
+                            />
+                        ))}
                     </div>
                 </ContainerMain>
             </div>
