940cd76603e5b79541b9167bf0374cabcd7ebbd4
Danil <danil.polovinkin.1994@gmail.com>
2023-08-10 15:26:52 +0300

    added some pagination

--- src/components/common/Pagination/Pagination.js
+++ src/components/common/Pagination/Pagination.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Pagination.scss'
+import { v1 } from 'uuid';
+import leftArrow from '../../../assets/icons/ArrowLeft.svg'
+import rightArrow from '../../../assets/icons/arrowRight.svg'
 
-const Pagination = () => {
+const Pagination = ({ postsPerPage, totalPosts, currentPage, paginate }) => {
+	const pageNumbers = [];
+
+	for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+		pageNumbers.push(i);
+	}
+	console.log(currentPage)
 	return (
-		<div>
+		<ul className="pagination-container">
+			<li onClick={() => (currentPage - 1 > 0) ? paginate(currentPage - 1) : ''} className='pagination-item'>
+				<img src={leftArrow} />
+			</li>
+			{pageNumbers.map(el => <li onClick={() => paginate(el)} className='pagination-item' key={v1()}>{el}</li>)}
+			<li onClick={() => paginate(currentPage + 1)} className='pagination-item'>
+				<img src={rightArrow} />
+			</li>
+		</ul>
 
-		</div>
 	);
 };
 
--- src/components/pages/Teachers/TeacherComment/TeacherComment.jsx
+++ src/components/pages/Teachers/TeacherComment/TeacherComment.jsx
@@ -6,42 +6,59 @@ import avatar from '../../../../assets/images/teacher/avatarCircle.png';
 import avatarW from '../../../../assets/images/teacher/avatarCircle.png?as=webp';
 import Picture from '../../../UI/Picture/Picture';
 import { IconRenderer } from '../../../UI/IconRenderer/IconRenderer';
+import Pagination from '../../../common/Pagination/pagination';
 
 const TeacherComment = () => {
     let obj = useFetch('reviews/');
-    const [blogPosts, setBlogPosts] = useState([obj?.data?.data[0].attributes.review]);
+    // const [blogPosts, setBlogPosts] = useState(obj?.data?.data[0].attributes.review);
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage, setPostsPerPage] = useState(2);
 
-    const indexOfLastPost = currentPage * postsPerPage,
-        indexOfFirstPost = indexOfLastPost - postsPerPage,
-        currentPosts = blogPosts.slice(indexOfFirstPost, indexOfLastPost);
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    // obj?.data?.data[0].attributes.review.slice(indexOfFirstPost, indexOfLastPost);
+    let currentPosts;
+    console.log(currentPosts);
 
-    console.log(obj?.data?.data[0].attributes.review);
+    currentPosts = [obj?.data?.data[0].attributes.review.slice(indexOfFirstPost, indexOfLastPost)];
+    console.log(currentPosts[0]);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     return (
         <div className='teacher__comments'>
             <div className='comments_wr'>
-                {obj?.data?.data[0].attributes.review.map((el) => (
-                    <div key={v1()} className='comment_item'>
-                        <div className='comment_top'>
-                            <Picture className={'comment_avatar'} img={avatar} webp={avatarW} />
-                            <div className='comment_name-wr'>
-                                <p className='comment_name'>{el.name}</p>
-                                <p className='comment_lessonNum'>
-                                    Кол-во уроков: <span>{el.lessonNum}</span>
-                                </p>
+                {obj?.data?.data[0].attributes.review
+                    .slice(indexOfFirstPost, indexOfLastPost)
+                    .map((el) => (
+                        <div key={v1()} className='comment_item'>
+                            <div className='comment_top'>
+                                <Picture className={'comment_avatar'} img={avatar} webp={avatarW} />
+                                <div className='comment_name-wr'>
+                                    <p className='comment_name'>{el.name}</p>
+                                    <p className='comment_lessonNum'>
+                                        Кол-во уроков: <span>{el.lessonNum}</span>
+                                    </p>
+                                </div>
+                            </div>
+                            <p className='comment_descr'>{el.descr}</p>
+                            <div className='comment_bottom-wr'>
+                                <p className='comment_date'>{el.date}</p>
+                                <div className='comment_likes-wr'>
+                                    <IconRenderer id={'thumbUp'} className='thumbUp' />
+                                    <p className='comment_like'>{el.likes}</p>
+                                </div>
                             </div>
                         </div>
-                        <p className='comment_descr'>{el.descr}</p>
-                        <div className='comment_bottom-wr'>
-                            <p className='comment_date'>{el.date}</p>
-                            <div className='comment_likes-wr'>
-                                <IconRenderer id={'thumbUp'} className='thumbUp' />
-                                <p className='comment_like'>{el.likes}</p>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+                    ))}
+                <Pagination
+                    currentPage={currentPage}
+                    postsPerPage={postsPerPage}
+                    totalPosts={obj?.data?.data[0].attributes.review.length}
+                    paginate={paginate}
+                />
             </div>
         </div>
     );
