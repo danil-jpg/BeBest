27c15ae30cceeeca96e35bb49c381390ea4a0c91
Danil <danil.polovinkin.1994@gmail.com>
2023-08-11 15:24:37 +0300

    some changes

--- src/store/slices/teachreFilterSlice/teacherFilterSlice.js
+++ src/store/slices/teachreFilterSlice/teacherFilterSlice.js
@@ -6,13 +6,16 @@ const teacherFilterSlice = createSlice({
 	name: 'teacherFilterSlice',
 	initialState: init,
 	reducers: {
-		setTeacherFilterSlice: (state, action) => {
-			// obj?.data?.data[0].attributes.review.sort((p1, p2) => {
-			// 	return p1.lessonNum < p2.lessonNum ? 1 : p1.lessonNum > p2.lessonNum ? -1 : 0;
-			// });
+		setUserList: (state, action) => {
+			state.users = action.payload
 		}
+		// setTeacherFilterSlice: (state, action) => {
+		// obj?.data?.data[0].attributes.review.sort((p1, p2) => {
+		// 	return p1.lessonNum < p2.lessonNum ? 1 : p1.lessonNum > p2.lessonNum ? -1 : 0;
+		// });
+		// }
 	}
 })
 
-export const { setTeacherFilterSlice } = teacherFilterSlice.actions
+export const { setUserList } = teacherFilterSlice.actions
 export default teacherFilterSlice.reducer;
