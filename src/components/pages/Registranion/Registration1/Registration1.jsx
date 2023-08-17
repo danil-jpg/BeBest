88d053effe945c8dbff56ba76f00b2bbe7e9a93b
Danil <danil.polovinkin.1994@gmail.com>
2023-08-17 10:35:32 +0300

    completed reg proposal

--- src/components/pages/Registranion/Registration1/Registration1.jsx
+++ src/components/pages/Registranion/Registration1/Registration1.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Registration1.scss';
 import Title from '../../../UI/Title/Title';
 import Picture from '../../../UI/Picture/Picture';
@@ -10,25 +10,43 @@ import working from '../../../../assets/images/article/working.png';
 import workingW from '../../../../assets/images/article/working.png?as=webp';
 
 const Registration1 = () => {
+    const [choosen, setChoosen] = useState([false, false, false]);
+
+    const onClickHandler = (par1, par2, par3) => setChoosen([par1, par2, par3]);
+
     return (
-        <div className='registration smallContainer'>
+        <div className='registration registration-proposal-container'>
             <Title>Регистрация</Title>
             <form className='registration__form'>
-                <div className='registration__choose'>
-                    <div className='registration__choose__circle'></div>
-                    <Picture img={working} webp={workingW} />
-                    <p className='registration__choose__text'>Ученик</p>
-                </div>
-                <div className='registration__choose'>
-                    <div className='registration__choose__circle'></div>
-                    <Picture img={teacher} webp={teacherW} />
-                    <p className='registration__choose__text'>Преподаватель</p>
-                </div>
-                <div className='registration__choose'>
-                    <div className='registration__choose__circle'></div>
-                    <Picture img={goal} webp={goalW} />
-                    <p className='registration__choose__text'>Компания</p>
-                </div>
+                <ul className='registration__ul'>
+                    <li
+                        onClick={() => onClickHandler(true, false, false)}
+                        className={
+                            choosen[0] ? 'registration__choose active' : 'registration__choose'
+                        }>
+                        <div className='registration__choose__circle'></div>
+                        <Picture img={working} webp={workingW} className={'registration__image'} />
+                        <p className='registration__choose__text'>Ученик</p>
+                    </li>
+                    <li
+                        onClick={() => onClickHandler(false, true, false)}
+                        className={
+                            choosen[1] ? 'registration__choose active' : 'registration__choose'
+                        }>
+                        <div className='registration__choose__circle'></div>
+                        <Picture img={teacher} webp={teacherW} className={'registration__image'} />
+                        <p className='registration__choose__text'>Преподаватель</p>
+                    </li>
+                    <li
+                        onClick={() => onClickHandler(false, false, true)}
+                        className={
+                            choosen[2] ? 'registration__choose active' : 'registration__choose'
+                        }>
+                        <div className='registration__choose__circle'></div>
+                        <Picture img={goal} webp={goalW} className={'registration__image'} />
+                        <p className='registration__choose__text'>Компания</p>
+                    </li>
+                </ul>
             </form>
         </div>
     );
