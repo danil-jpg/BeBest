a8f32c048ccba9a8c061e9f091399c8da542a9bd
Danil <danil.polovinkin.1994@gmail.com>
2023-08-28 16:42:34 +0300

    ...

---
+++ src/webSocket.jsx
@@ -0,0 +1,11 @@
+// import Websocket from 'ws';
+
+// const wss = new Websocket.Server({ port: '8080' });
+
+// wss.on('connection', () => {
+//     console.log('Start');
+
+//     wss.on('close', () => {
+//         console.log('End');
+//     });
+// });
