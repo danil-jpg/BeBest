2654696f61583ae74bc5f520e4ef2bfa8a7ea58a
Danil <danil.polovinkin.1994@gmail.com>
2023-08-29 13:45:51 +0300

    some changes

--- src/components/pages/Account/Chat/Chat.jsx
+++ src/components/pages/Account/Chat/Chat.jsx
@@ -4,23 +4,23 @@ import './Chat.scss';
 
 const Chat = () => {
     return (
-        <div className='chat'>
-            <div className='chat__left'>
-                <div className='chat__group'>
-                    <div className='chat__item'>
-                        <div className='chat__item__left'>
-                            <img src='https://thumbs.dreamstime.com/z/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg?w=768' />
-                        </div>
-                        <div className='chat__item__right'>
-                            <p className='chat__name'>xxxx</p>
-                            <p className='chat__last-text'>xxxx</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='chat__right'></div>
-            {/* <LongPulling></LongPulling> */}
-        </div>
+        // <div className='chat'>
+        //     <div className='chat__left'>
+        //         <div className='chat__group'>
+        //             <div className='chat__item'>
+        //                 <div className='chat__item__left'>
+        //                     <img src='https://thumbs.dreamstime.com/z/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg?w=768' />
+        //                 </div>
+        //                 <div className='chat__item__right'>
+        //                     <p className='chat__name'>xxxx</p>
+        //                     <p className='chat__last-text'>xxxx</p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className='chat__right'></div>
+        <LongPulling></LongPulling>
+        // </div>
     );
 };
 
