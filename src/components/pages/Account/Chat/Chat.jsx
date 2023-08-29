6a2c252d1f264b63c37c95c48e3404b6b1270641
Danil <danil.polovinkin.1994@gmail.com>
2023-08-29 14:55:04 +0300

    some chat work

--- src/components/pages/Account/Chat/Chat.jsx
+++ src/components/pages/Account/Chat/Chat.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import LongPulling from './LongPulling';
 import './Chat.scss';
+import { useState } from 'react';
+import { v1 } from 'uuid';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { IconRenderer } from '../../../UI/IconRenderer/IconRenderer.jsx';
 
 const Chat = () => {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+
+    const sendMessageHandler = async () => {
+        await axios.post('http://localhost:5000/new-messages', {
+            message: value,
+        });
+    };
+
+    useEffect(() => {
+        subscribe();
+    }, []);
+
+    const subscribe = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:5000/get-messages');
+            setMessages((prev) => [data, ...prev]);
+            await subscribe();
+        } catch (error) {
+            setTimeout(() => {
+                subscribe();
+            }, 500);
+        }
+    };
+
     return (
         <div className='chat'>
             <div className='chat__left'>
@@ -22,7 +51,27 @@ const Chat = () => {
                 <div className='chat__right_top'>
                     <p className='chat__right_top_text'>Общий чат</p>
                 </div>
-                <LongPulling></LongPulling>
+                <div className='chat__body'>
+                    <div className='messages'>
+                        {messages.map((el) => (
+                            <div key={v1()} className='messages__item'>
+                                {el.message}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className='longPulling'>
+                    <div className='messages__input-wr'>
+                        <input
+                            className='messages__input'
+                            value={value || ''}
+                            onChange={(e) => setValue(e.target.value)}
+                        />
+                        <div className='messages__send' onClick={sendMessageHandler}>
+                            <IconRenderer id='send' />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
