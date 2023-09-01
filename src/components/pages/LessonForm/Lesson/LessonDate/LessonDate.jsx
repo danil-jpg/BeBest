309cfba7b53f74a7016372292be3a0913492d4a0
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-09-01 09:13:31 +0300

    update lesson Date

--- src/components/pages/LessonForm/Lesson/LessonDate/LessonDate.jsx
+++ src/components/pages/LessonForm/Lesson/LessonDate/LessonDate.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import './LessonDate.scss';
 import iconCalendar from "../../../../../assets/images/lesson/calendar.svg"
-const LessonDate = ({size}) => {
+const LessonDate = ({ size }) => {
   const [selectedDate, setSelectedDate] = useState(null);
 
   const handleDateChange = (date) => {
@@ -12,7 +12,7 @@ const LessonDate = ({size}) => {
 
   return (
     <div className='lesson-date'>
-      
+
       <DatePicker className={`lesson-date__main lesson-date__main-${size}`} selected={selectedDate} onChange={handleDateChange} />
       <img className='lesson-date__icon' src={iconCalendar} alt="" />
     </div>
